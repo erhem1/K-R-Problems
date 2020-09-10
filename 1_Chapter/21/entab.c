cd775a3a3eb90f4ba5234d2f5217d722b16eabc8
RunninPekkin <erhembaatardashdorj@gmail.com>
2020-09-10 19:18:14 +0000

    1.22

--- 1_Chapter/21/entab.c
+++ 1_Chapter/21/entab.c
@@ -4,11 +4,11 @@ int entab(char line[], int len);
 int main(void)
 {
 	char line[LIMIT];
-	int len;
+	int len, new_len;
 	int c;
 	do 
 	{
-		for(len = 0; len < LIMIT - 1 && (c = getchar()) != EOF && c != '\n')
+		for(len = 0; len < LIMIT - 1 && (c = getchar()) != EOF && c != '\n'; len++)
 		{
 			line[len] = c;	
 		}
@@ -18,21 +18,26 @@ int main(void)
 			len++;
 		}
 		line[len] = '\0';
+		printf("%d \n", len);
+		new_len = entab(line, len);
+		printf("%s", line);
+		printf("%d \n", new_len);
 	} while(len > 0);
-
-	entab(line, len);	
-	printf("%s", line);
 	return 0;
 }
 
 int entab(char line[], int len)
 {
-	int space_count;
+	int space_count = 0;
 	for(int i = 0; i < len; i++)
 	{
-		if(line[i] = ' ')
+		if(line[i] == ' ')
 		{
 			space_count++;
+		}
+		else
+		{
+			space_count = 0;
 		}
 		
 		if(space_count == COL)
@@ -41,7 +46,9 @@ int entab(char line[], int len)
 			{
 				line[j-(COL-1)] = line[j];	
 			}
+			i -= 7;
 			len -= 7;
 		}
 	}	
+	return len;
 }
